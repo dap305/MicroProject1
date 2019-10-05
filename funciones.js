eec11c70e78128a15dced2d1a383aa7d40ed5de7
dap305 <da.prieto@correo.unimet.edu.ve>
2019-10-05 01:22:29 -0400

    listolin

---
+++ funciones.js
@@ -0,0 +1,15 @@
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+}
